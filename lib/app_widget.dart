b349b8d8f56b53b5c243c2756b35d00b3d42ba83
HicaroD <hdanrlley1@gmail.com>
2023-01-25 11:23:17 -0300

    proj: adding flutter app

---
+++ lib/app_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello, world!");
+  }
+}
