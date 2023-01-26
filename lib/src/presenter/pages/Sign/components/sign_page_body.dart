52b68e0c09ebae8608913db572b4197c86c064fb
HicaroD <hdanrlley1@gmail.com>
2023-01-26 20:08:22 -0300

    client(Sign): creating widget for representing body of Sign page

---
+++ lib/src/presenter/pages/Sign/components/sign_page_body.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+import '../../../widgets/Buttons/primary_button.dart';
+import '../../../widgets/Buttons/secondary_button.dart';
+import '../../../widgets/FormManager/form_field_content.dart';
+import '../../../widgets/FormManager/form_manager.dart';
+
+class SignPageBody extends StatefulWidget {
+  final Size screenSize;
+
+  const SignPageBody({
+    Key? key,
+    required this.screenSize,
+  }) : super(key: key);
+
+  @override
+  State<SignPageBody> createState() => _SignPageBodyState();
+}
+
+class _SignPageBodyState extends State<SignPageBody> {
+  Size get screenSize => widget.screenSize;
+
+  final signInForms = FormManager(
+    formKey: GlobalKey<FormState>(),
+    formContent: <String, FormFieldContent>{
+      "email": FormFieldContent(
+        labelText: "E-mail",
+        hintText: "Entre com seu e-mail",
+      ),
+      "password": FormFieldContent(
+        labelText: "Senha",
+        hintText: "Digite sua senha",
+      ),
+    },
+  );
+
+  final signUpForms = FormManager(
+    formKey: GlobalKey<FormState>(),
+    formContent: <String, FormFieldContent>{
+      "name": FormFieldContent(
+        labelText: "Nome",
+        hintText: "Digite seu nome completo",
+      ),
+      "username": FormFieldContent(
+        labelText: "Nome de usuário",
+        hintText: "Digite seu nome de usuário",
+      ),
+      "email": FormFieldContent(
+        labelText: "E-mail",
+        hintText: "Entre com seu e-mail",
+      ),
+      "password": FormFieldContent(
+        labelText: "Senha",
+        hintText: "Digite sua senha",
+      ),
+    },
+  );
+
+  bool _isSignInForm = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            SecondaryButton(
+              "Entrar",
+              onPressed: () {
+                if (!mounted) return;
+                setState(() {
+                  _isSignInForm = true;
+                });
+              },
+              hasBottomBorder: _isSignInForm,
+            ),
+            SecondaryButton(
+              "Registrar",
+              onPressed: () {
+                if (!mounted) return;
+                setState(() {
+                  _isSignInForm = false;
+                });
+              },
+              hasBottomBorder: !_isSignInForm,
+            ),
+          ],
+        ),
+        SizedBox(
+          width: screenSize.width * 0.8,
+          child: Column(
+            children: <Widget>[
+              _isSignInForm
+                  ? signInForms.buildForms()
+                  : signUpForms.buildForms(),
+              SizedBox(
+                width: 150,
+                child: PrimaryButton(
+                  onPressed: () {
+                    Map<String, String> data = signInForms.data();
+                    if (!_isSignInForm) {
+                      data = signUpForms.data();
+                    }
+                    print(data);
+                  },
+                  text: _isSignInForm ? "Entrar" : "Registrar",
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    signInForms.disposeControllers();
+    signUpForms.disposeControllers();
+    super.dispose();
+  }
+}
--- lib/src/presenter/pages/Sign/sign.dart
+++ lib/src/presenter/pages/Sign/sign.dart
@@ -1,57 +1,11 @@
 import 'package:flutter/material.dart';
 
-import '../../widgets/Buttons/primary_button.dart';
-import '../../widgets/Buttons/secondary_button.dart';
-import '../../widgets/FormManager/form_field_content.dart';
-import '../../widgets/FormManager/form_manager.dart';
+import 'components/sign_page_body.dart';
 import 'components/sign_header.dart';
 import '../../../utils/colors.dart';
 
-class SignPage extends StatefulWidget {
+class SignPage extends StatelessWidget {
   const SignPage({Key? key}) : super(key: key);
-
-  @override
-  State<SignPage> createState() => _SignPageState();
-}
-
-class _SignPageState extends State<SignPage> {
-  final signInForms = FormManager(
-    formKey: GlobalKey<FormState>(),
-    formContent: <String, FormFieldContent>{
-      "email": FormFieldContent(
-        labelText: "E-mail",
-        hintText: "Entre com seu e-mail",
-      ),
-      "password": FormFieldContent(
-        labelText: "Senha",
-        hintText: "Digite sua senha",
-      ),
-    },
-  );
-
-  final signUpForms = FormManager(
-    formKey: GlobalKey<FormState>(),
-    formContent: <String, FormFieldContent>{
-      "name": FormFieldContent(
-        labelText: "Nome",
-        hintText: "Digite seu nome completo",
-      ),
-      "username": FormFieldContent(
-        labelText: "Nome de usuário",
-        hintText: "Digite seu nome de usuário",
-      ),
-      "email": FormFieldContent(
-        labelText: "E-mail",
-        hintText: "Entre com seu e-mail",
-      ),
-      "password": FormFieldContent(
-        labelText: "Senha",
-        hintText: "Digite sua senha",
-      ),
-    },
-  );
-
-  bool _isSignInForm = true;
 
   @override
   Widget build(BuildContext context) {
@@ -64,56 +18,10 @@ class _SignPageState extends State<SignPage> {
         child: Column(
           children: <Widget>[
             const SignPageHeader(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                SecondaryButton(
-                  "Entrar",
-                  onPressed: () {
-                    setState(() {
-                      _isSignInForm = true;
-                    });
-                  },
-                  hasBottomBorder: _isSignInForm,
-                ),
-                SecondaryButton(
-                  "Registrar",
-                  onPressed: () {
-                    setState(() {
-                      _isSignInForm = false;
-                    });
-                  },
-                  hasBottomBorder: !_isSignInForm,
-                ),
-              ],
-            ),
-            SizedBox(
-              width: screenSize.width * 0.8,
-              child: Column(
-                children: <Widget>[
-                  _isSignInForm
-                      ? signInForms.buildForms()
-                      : signUpForms.buildForms(),
-                  SizedBox(
-                    width: 150,
-                    child: PrimaryButton(
-                      onPressed: () => print("CLICKING"),
-                      text: _isSignInForm ? "Entrar" : "Registrar",
-                    ),
-                  )
-                ],
-              ),
-            ),
+            SignPageBody(screenSize: screenSize),
           ],
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    signInForms.disposeControllers();
-    signUpForms.disposeControllers();
   }
 }
