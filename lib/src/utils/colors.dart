33e965caa7d4d2ae7f9f8e744b8acef5fd90339d
HicaroD <hdanrlley1@gmail.com>
2023-01-26 15:13:34 -0300

    client(utils): building folder for storing util files

---
+++ lib/src/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PomoColors {
+  static const PRIMARY_RED = Color(0xfff35d4f);
+  static const SECONDARY_RED = Color(0xFFD9665B);
+  static const PRIMARY_WHITE = Colors.white;
+  static const PRIMARY_GREY = Color(0xFFD9D9D9);
+}
