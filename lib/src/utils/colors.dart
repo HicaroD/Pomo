684c361c2668c7598329b0bb7e3d6d1e17621dcf
HicaroD <hdanrlley1@gmail.com>
2023-01-28 20:40:28 -0300

    colors: adding primary and secondary green

--- lib/src/utils/colors.dart
+++ lib/src/utils/colors.dart
@@ -6,5 +6,6 @@ class PomoColors {
   static const PRIMARY_WHITE = Color(0xFFF2F2F2);
   static const PRIMARY_GREY = Color(0xFFBFBFBF);
   static const PRIMARY_BLACK = Color(0xFF0D0D0D);
-  static const PRIMARY_GREEN = Color(0xFF8ABF3B);
+  static const PRIMARY_GREEN = Color(0xFF1BE35A);
+  static const SECONDARY_GREEN = Color(0xFF8ABF3B);
 }
