95b3818646fab07c14063f1ca0945e6cc35dc3cb
HicaroD <hdanrlley1@gmail.com>
2023-01-28 20:30:29 -0300

    colors: inverting colors

--- lib/src/utils/colors.dart
+++ lib/src/utils/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class PomoColors {
-  static const PRIMARY_RED = Color(0xFFD9665B);
-  static const SECONDARY_RED = Color(0xFFEC665B);
+  static const SECONDARY_RED = Color(0xFFD9665B);
+  static const PRIMARY_RED = Color(0xFFEC665B);
   static const PRIMARY_WHITE = Colors.white;
   static const PRIMARY_GREY = Color(0xFFD9D9D9);
 }
