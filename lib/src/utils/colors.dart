537c49ee4f6b1a94dddbea33bafe5d40fe5aba3f
HicaroD <hdanrlley1@gmail.com>
2023-01-28 20:38:24 -0300

    colors: changing primary white and primary grey colors

--- lib/src/utils/colors.dart
+++ lib/src/utils/colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class PomoColors {
   static const SECONDARY_RED = Color(0xFFD9665B);
   static const PRIMARY_RED = Color(0xFFEC665B);
-  static const PRIMARY_WHITE = Colors.white;
-  static const PRIMARY_GREY = Color(0xFFD9D9D9);
+  static const PRIMARY_WHITE = Color(0xFFF2F2F2);
+  static const PRIMARY_GREY = Color(0xFFBFBFBF);
 }
