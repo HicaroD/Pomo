5f819bdfc5bb89c2e7509785fc3d07ca85d3cb27
HicaroD <hdanrlley1@gmail.com>
2023-02-04 20:23:55 -0300

    feat(#8): file for storing endpoints

---
+++ lib/src/utils/api_endpoints.dart
@@ -0,0 +1,9 @@
+const String BASE_URL = "https://pomo-api.onrender.com";
+
+const String UPDATE_USER_ENDPOINT = "/user/update/";
+
+const String REMOVE_USER_ENDPOINT = "/user/remove/";
+
+const String CREATE_USER_ENDPOINT = "/user/create/";
+
+const String LOGIN_ENDPOINT = "/auth";
